a1e40a0a81f299aae65fce9e3718e106c77b9dff
kwagalashalom <99474758+kwagalashalom@users.noreply.github.com>
2024-02-24 05:12:26 -0800

    added title

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,12 @@
-export default function App() {
-    return (
-      <h1 className="text-3xl font-bold underline text-red-600">
-        Hello world!
-      </h1>
-    )
-  }
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='p-5'>
+        <h1 className='text-slate-500 font-bold uppercase text-center text-xl'>STORES</h1>
+
+     </div>
+  )
+}
+
+export default App
